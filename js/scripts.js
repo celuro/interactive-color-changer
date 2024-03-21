232bc55f269b5b76f525d3bd04063b74504a7a8c
Margaret <margaret.ach7@gmail.com>
2024-03-21 01:57:46 -0400

    finished script, color changes

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//First we need to create a button in order for the background to change
+let button = document.createElement("button");
+document.body.appendChild(button);
+
+//Next we need to define a list of colors. I will use magenta, aqua, indigo and light pink.
+
+const colors = ["#de268e", "#26dec2", "#1a113d", "#f59fbd"];
+
+button.addEventListener("click", function(){
+  //We need to define an index that represents the randomized color
+  let index = Math.floor(Math.random() * colors.length);
+
+  /*
+  Now that we can use colors[index] to represent a random color on click,
+  we will add an eent handler for when the button is clicked to change the
+  background color.
+  */
+  document.documentElement.style.backgroundColor = colors[index];
+});
+
+
